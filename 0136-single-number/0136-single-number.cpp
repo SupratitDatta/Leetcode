a24799a3b6b63abe66bd847850bb7103e1fac3a3
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-07 01:36:21 +0530

    Time: 27 ms (13.08%), Space: 23.7 MB (5.62%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if(nums.size() == 1) {
-            return nums[0];
+        unordered_map<int,int> x;
+	    for(auto it: nums){
+		   x[it]++;
         }
-        for(int i=0; i<nums.size()-1; i+=2) {
-            if(nums[i] != nums[i+1]) {
-                return nums[i];
+	    for(auto i:x){
+		    if(i.second==1){
+			    return i.first;
             }
         }
-        return nums[nums.size()-1];
+	   return -1;
     }
 };
