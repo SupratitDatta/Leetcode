57ded295f48c2addd963b1067319b67416c1876c
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-07 01:37:25 +0530

    Time: 6 ms (97.21%), Space: 19.1 MB (98.71%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int x=0;
+        int a=0;
         for(int i=0; i<nums.size(); i++){
-            x=x^nums[i];
+            a=a^nums[i];
         }
-        return x;
+        return a;
     }
 };
