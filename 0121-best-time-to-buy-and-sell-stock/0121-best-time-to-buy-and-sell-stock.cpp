493c8ed052206035d55503e6d98eb3f802160b92
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-08 00:21:30 +0530

    Time: 81 ms (78.39%), Space: 95.9 MB (31.23%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int m=prices[0],p=0;
+        int m=prices[0],pr=0;
         for(int i=1; i<prices.size(); i++){
             int c = prices[i] - m;
-            p = max(p,c);
+            pr = max(pr,c);
             m = min(m,prices[i]);
         }
-        return p;
+        return pr;
     }
 };
