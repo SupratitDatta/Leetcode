feed1507f700a04ada4d8277fbc7061a3fd331a2
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-08 15:36:22 +0530

    Time: 4 ms (58.29%), Space: 14.6 MB (56.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         // next_permutation(nums.begin(), nums.end());
-        int k = -1, n = nums.size();
+        int index = -1, n = nums.size();
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                k = i;
+                index = i;
                 break;
             }
         }
-        if (k == -1) {
+        if (index == -1) {
             reverse(nums.begin(), nums.end());
         } 
         else {
-            for (int i = n - 1; i > k; i--) {
-                if (nums[i] > nums[k]) {
-                    swap(nums[i], nums[k]);
+            for (int i = n - 1; i > index; i--) {
+                if (nums[i] > nums[index]) {
+                    swap(nums[i], nums[index]);
                     break;
                 }
             }
 
-            reverse(nums.begin() + k + 1, nums.end());
+            reverse(nums.begin() + index + 1, nums.end());
         }
     }
 };
