36fc810bc70a5af27416155bcbf452e41ea986fd
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-09 11:46:39 +0530

    Time: 55 ms (91.38%), Space: 45.1 MB (46.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int c = 0;
-        int sum = 0;
+        int c = 0,sum = 0;
         unordered_map<int, int> x; 
         x[0] = 1;
 
@@ -13,7 +12,8 @@ public:
             }
             if (x.find(sum) != x.end()) {
                 x[sum]++;
-            } else {
+            } 
+            else {
                 x[sum] = 1;
             }
         }
