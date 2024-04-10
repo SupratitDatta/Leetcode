95e86cf284edef2f259290b2a4c7483897c61b07
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-10 20:02:59 +0530

    Time: 0 ms (100%), Space: 10.9 MB (95.35%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for (int j=0, i=m; j<n; j++){
-            n1[i] = n2[j];
+            nums1[i] = nums2[j];
             i++;
         }
-        sort(n1.begin(),n1.end());
+        sort(nums1.begin(),nums1.end());
     }
 };
