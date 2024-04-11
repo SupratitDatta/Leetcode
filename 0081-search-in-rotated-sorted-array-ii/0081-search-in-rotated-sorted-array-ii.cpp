33997f843f41aecc7834bc7f41e5360f0edb632d
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-11 12:11:05 +0530

    Time: 6 ms (34.16%), Space: 16.5 MB (28.99%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,25 +8,25 @@ public:
             if (nums[mid] == target){
                 return true;
             }
-            if (nums[s] < nums[mid]){
-                if (nums[s] <= target && target <= nums[mid]){
-                    e = mid-1;
-                } 
-                else{
-                    s = mid+1;
-                }
+            if(nums[s] == nums[mid] && nums[mid] == nums[e]){
+                s++;
+                e--;
             } 
-            else if(nums[mid] < nums[e]){
-                if(nums[mid] <= target && target <= nums[e]) {
-                    s = mid+1;
+            else if(nums[s] <= nums[mid]){
+                if(nums[s] <= target && target <= nums[mid]){
+                    e = mid - 1;
                 } 
                 else{
-                    e = mid-1;
+                    s = mid + 1;
                 }
             } 
             else{
-                s++;
-                e--;
+                if(nums[mid] <= target && target <= nums[e]){
+                    s = mid + 1;
+                } 
+                else{
+                    e = mid - 1;
+                }
             }
         }
         return false;
