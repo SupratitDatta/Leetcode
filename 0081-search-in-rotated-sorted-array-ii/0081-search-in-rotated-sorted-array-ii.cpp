d577b3eac0dae8291e34f832770cbbca80e080ba
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-11 12:11:11 +0530

    Time: 7 ms (31.92%), Space: 16.5 MB (71.32%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int s = 0, e = nums.size()-1;
+        int s=0, e=nums.size()-1;
+        
+        while (s <= e) {
+            int mid = s+(e-s)/2;
 
-        while (s <= e){
-            int mid = s + (e-s)/2;
-            if (nums[mid] == target){
-                return true;
+            if (nums[mid] == target) return true;
+
+            if (nums[s] == nums[mid] && nums[mid] == nums[e]) {
+                s = s + 1;
+                e = e - 1;
+                continue;
             }
-            if(nums[s] == nums[mid] && nums[mid] == nums[e]){
-                s++;
-                e--;
-            } 
-            else if(nums[s] <= nums[mid]){
-                if(nums[s] <= target && target <= nums[mid]){
+
+            if (nums[s] <= nums[mid]) {
+                if (nums[s] <= target && target <= nums[mid]){
                     e = mid - 1;
-                } 
+                }
                 else{
                     s = mid + 1;
                 }
-            } 
+            }
             else{
-                if(nums[mid] <= target && target <= nums[e]){
+                if (nums[mid] <= target && target <= nums[e]){
                     s = mid + 1;
-                } 
+                }
                 else{
                     e = mid - 1;
                 }
