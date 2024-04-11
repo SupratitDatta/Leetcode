93bd2d7c4da598452d02558bb0a1261ccf84c84a
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-11 13:28:20 +0530

    Time: 15 ms (85.64%), Space: 24.6 MB (51.04%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int s=0, e=nums.size()-1, mid;
+
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.size()-1] != nums[nums.size()-2]){
+            return nums[nums.size()-1];
+        }
+        
+        while(s<=e){
+            mid = s+(e-s)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if( (mid%2 == 0 && nums[mid] == nums[mid+1]) || 
+                (mid%2 == 1 && nums[mid] == nums[mid-1]) ){
+                    s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return -1;
+    }
+};
