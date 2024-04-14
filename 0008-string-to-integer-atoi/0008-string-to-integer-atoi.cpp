ae83bd4e019170845a6476fb832fe543e3c7fabd
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-14 22:46:00 +0530

    Time: 3 ms (57.91%), Space: 8.2 MB (67.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        return stoi(s);
+        int i=0, x=1;
+        long k=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-'){
+            x = -1;
+            i++;
+        }
+        else if(s[i] == '+'){
+            i++;
+        }
+        while(i<s.length()){
+            if(s[i] >= '0' && s[i] <= '9'){
+                k=k*10+(s[i]-'0');
+                if(k>INT_MAX && x==-1){
+                    return INT_MIN;
+                }
+                else if(k > INT_MAX && x == 1){
+                    return INT_MAX;
+                }
+                i++;
+            }
+            else{
+                return k*x;
+            }
+        }
+        return (k*x);
     }
 };
