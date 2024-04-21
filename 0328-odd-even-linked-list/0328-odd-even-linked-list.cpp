23d764694721e16e1145f5610d5e613a4cda9637
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-21 14:00:14 +0530

    Time: 4 ms (91.24%), Space: 14 MB (77.15%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -22,11 +22,10 @@ public:
         while (even != nullptr && even->next != nullptr) {
             odd->next = even->next; 
             odd = odd->next;
-
+            
             even->next = odd->next; 
             even = even->next;
         }
-
         odd->next = evenHead; 
 
         return head;
