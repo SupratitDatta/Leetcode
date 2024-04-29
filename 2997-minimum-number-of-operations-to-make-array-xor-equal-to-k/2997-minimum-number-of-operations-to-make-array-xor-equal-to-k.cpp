dccd5c56369d29fc74f3e86c8ec0b4c1f55e6a03
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2024-04-29 14:34:49 +0530

    Time: 100 ms (86.04%), Space: 91.4 MB (25.13%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        bitset<20> xorSum=k;
-        for(int x: nums)
-            xorSum^=x;
-        return xorSum.count();
+        int x = 0;
+        for (int n : nums) {
+            x = x ^ n;
+        }
+        int count = 0;
+        while (k || x) {
+            if ((k % 2) != (x % 2)) {
+                count++;
+            }
+            
+            k /= 2;
+            x /= 2;
+        }
+        
+        return count;
     }
 };
